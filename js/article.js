07871d9aa133dd1afd51fde973b6d013a11444fc
Fedor Pavlov <84475875+zencue@users.noreply.github.com>
2025-03-04 11:31:05 -0500

    Update article.js

--- js/article.js
+++ js/article.js
@@ -1,8 +1,15 @@
-// let els = document.getElementsByClassName("article-element")
-// setTimeout(function(){
-//     for(let i =0;i<els.length;i++){
-//         els[i].style.animation = "fade 1s";
-//         els[i].style.opacity = "1"
-//     }
-
-//   },1000)
+title = document.getElementById("article-title");
+function anim(str,element){
+    let titleStr = Array.from(str);
+    let time = 0;
+    function frame() {
+    if (time == titleStr.length) {
+        clearInterval(id);
+    } else {
+        element.textContent =element.textContent+titleStr[time];
+        time++;
+    }
+    }
+    let id = setInterval(frame, 50);
+}
+anim("Article Title",title)
