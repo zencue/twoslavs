9bbf8b53173c6812b1f8e33d3df49627e3a1b4ea
Fedor Pavlov <84475875+zencue@users.noreply.github.com>
2025-03-03 22:02:20 -0500

    Add files via upload

---
+++ js/article.js
@@ -0,0 +1,8 @@
+// let els = document.getElementsByClassName("article-element")
+// setTimeout(function(){
+//     for(let i =0;i<els.length;i++){
+//         els[i].style.animation = "fade 1s";
+//         els[i].style.opacity = "1"
+//     }
+
+//   },1000)
