c1496e8c43c8835559ee7bfd9cf891d57a0a8d7a
zencue <fedotpvlv53@gmail.com>
2025-03-26 15:12:15 -0400

    some flaws

--- articles-data.js
+++ articles-data.js
@@ -1,6 +1,6 @@
 articlesData={
-    "catapult-and-ping-pong":{
-        "title":"Catapult and Ping Pong",
+    "the-catapult-and-the-ping-pong-ball":{
+        "title":"The Catapult and the Ping-pong Ball",
         "authors":["Fedor", "Tymofiy"],
         "date":"2025.03.23",
         "cover":"img/catapult.jpg",
